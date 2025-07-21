39c32adbd414bfd8af8d4b68fd4703ec1162fa9d
Paul Wu <59194790+bcwu324@users.noreply.github.com>
2025-07-21 17:48:58 +0800

    Add disk usage module and tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "meminfo.h"
 #include "cpuinfo.h"
+#include "diskusage.h"
 
 int main() {
     auto mem_values = parseMemInfo("/proc/meminfo");
     printMemoryInfo(mem_values);
+    auto disk_usage = getDiskUsage("/");
+    printDiskUsage(disk_usage);
     auto cpu_usage = getCpuUsage();
     std::cout << "CPU Usage: user " << cpu_usage.first
               << "% system " << cpu_usage.second << "%" << std::endl;
